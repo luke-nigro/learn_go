fdd11b800f824bd0bf8b59822c64ee31c4f14073
Luke Nigro <luke.nigro@outlook.com>
2024-06-04 10:07:37 -0400

    Added functionality to sum tails of variable length lists of integers

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -9,10 +9,24 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-	for i, numbers := range numbersToSum {
-		sums[i] += Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) int {
+	var tails []int
+	var tail int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			tail = 0
+		} else {
+			tail = numbers[len(numbers)-1]
+		}
+		tails = append(tails, tail)
+	}
+	tailSum := Sum(tails)
+	return tailSum
 }
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -19,12 +19,39 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 	t.Run("two collections of numbers", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("two collections of numbers, one empty", func(t *testing.T) {
+		got := SumAll([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, wanted %v", got, want)
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
 		}
+	}
+	t.Run("two collections of numbers", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := 11
+		checkSums(t, got, want)
+	})
+	t.Run("two collections of numbers, one empty", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := 9
+		checkSums(t, got, want)
 	})
 }
